b274267394b8e3418d0ec3e9fe1a830a5347925e
J3TLA9 <johnmichael.relova118@gmail.com>
2023-12-09 17:38:42 +0800

    Added mainmenu and rename

---
+++ main/main.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Main Menu:");
+                Console.WriteLine("[1] Store to ASEAN phonebook");
+                Console.WriteLine("[2] Edit entry in ASEAN phonebook");
+                Console.WriteLine("[3] Search ASEAN phonebook by country");
+                Console.WriteLine("[4] Exit");
+                Console.Write("Enter number of choice: ");
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            //to_ASEAN_phonebook();
+                            break;
+                        case 2:
+                            //edit_ASEAN_phonebook();
+                            break;
+                        case 3:
+                            //get_ASEAN_phonebook();
+                            break;
+                        case 4:
+                            return;
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                }
+            }
+        }
+    }
+}
